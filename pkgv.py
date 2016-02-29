ce08af54564eb08915263ab15d8b57b37a59fd05
volodja <bulaev_vladimir@mail.ru>
2016-02-29 20:04:09 +0600

    refactoring

--- pkgv.py
+++ pkgv.py
@@ -4,7 +4,9 @@
 import platform
 import sys
 import re
-import pkg
+
+
+# -----load pkg by platform
 
 
 def splitByUpCase(src):
@@ -24,54 +26,55 @@ if platform_name == '':
     platform_name = platform.system().lower()
 exec('import ' + platform_name + ' as pkg')
 
-
-def showPlatform():
-    print('platform: ' + platform_name)
-
-
-dict_command_function   = dict()
-dir_                    = dir(pkg)
+command_dictionary  = dict()
+dir_                = dir(pkg)
 for function_name in dir_:
     if function_name[:7] == 'execute':
-        command_name    = convertFunctionNameToParameterName(function_name[7:])            
-        dict_command_function[command_name] = function_name
-
+        command_name                        = \
+            convertFunctionNameToParameterName(function_name[7:])
+        command_dictionary[command_name]    = \
+            function_name
 
 commands = ''
-for command in dict_command_function.keys():
+for command in command_dictionary.keys():
     params = ''
     func_code = ''
-    exec('func_code = pkg.' + dict_command_function[command] + '.__code__')
+    exec('func_code = pkg.' + command_dictionary[command] + '.__code__')
 
     for param in func_code.co_varnames:
         params = params + param
 
     commands = commands + "    " + command + ' ' + params + "\n"
-
 help_text = "pkg\n    platform\n" + commands
 
 
-if len(sys.argv) <  2:
+# -----
+
+
+def executeCommand(command_dictionary, command, argument = None):
+    if command == 'platform' and argument is None:
+        print('platform: ' + platform_name)
+        exit()
+    else:
+        if command not in command_dictionary.keys():
+            print("unknown command: " + command + "\n")
+            print(help_text)
+            exit(1)
+
+        if argument is None:
+            exec('pkg.' + command_dictionary[command] + '()')
+        else:
+            exec('pkg.' + command_dictionary[command] + '(argument)')
+
+
+if len(sys.argv) < 2:
     print(help_text)
     exit(0)
-    
-
-command = sys.argv[1]
-
-
-if command != 'platform'  and  command not in dict_command_function.keys():
-    print("unknown command: " + command + "\n")
-    print(help_text)
-    exit(1)
 
 
 if len(sys.argv) == 2:
-    if command == 'platform':
-        showPlatform()
-        exit()
-    exec('pkg.' + dict_command_function[command] + '()')
-    
+    executeCommand(command_dictionary, sys.argv[1])
+
 
 if len(sys.argv) == 3:
-    name = sys.argv[2]
-    exec('pkg.' + dict_command_function[command] + '(name)')
+    executeCommand(command_dictionary, sys.argv[1], sys.argv[2])
